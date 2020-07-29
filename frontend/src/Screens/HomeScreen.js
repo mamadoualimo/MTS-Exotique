34108972f8ce59ec564e356be1d0f1ae64d72ab2
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2020-07-29 16:59:47 +0200

    Récupérer les données du serveur

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
 
 function HomeScreen(props) {
+
+  const [products, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get("/api/products");
+      setProduct(data);
+    }
+    fetchData();
+    return () => {
+      //
+    };
+  }, [])
+
     return <ul className="products">
     {
-      data.products.map(product => 
-        <li>
+      products.map(product => 
+        <li key={product._id}>
           <div class="product">
             <Link to={'/product/' + product._id}>
-              <img class="product-image" src={product.image} alt="product" />
+              <img className="product-image" src={product.image} alt="product" />
             </Link>
             <div class="product-name">
               <Link to={'/product/' + product._id}>{product.name}</Link>
